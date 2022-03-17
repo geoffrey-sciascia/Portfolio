a8b41493194d2630f4f8ad34f67e92508d24b464
Geoffrey-Sciascia <gwsciascia@gmail.com>
2022-03-18 12:19:09 +1300

    attempt install of react-bootstrap

--- client/components/Info.jsx
+++ client/components/Info.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import Container from './react-bootstrap/Container'
+import Row from './react-bootstrap/Row'
+import Col from './react-bootstrap/Col'
+
 function Info () {
-    return (
-        <h3>This will form my portfolio over time as I build it using React, Knex, State etc.</h3>
-    )
+  return (
+    <Container fluid>
+      <Row>
+        <Col>
+          <h3>This will form my portfolio over time as I build it using React, Knex, State etc.</h3>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default Info
