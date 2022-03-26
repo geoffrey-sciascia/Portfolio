737a302c7342b2aeeb43ba8c34fd3013522bbd3b
Geoffrey-Sciascia <gwsciascia@gmail.com>
2022-03-27 00:38:38 +1300

    add capitals to pronounciation seed data

--- server/db/seeds/hello.js
+++ server/db/seeds/hello.js
@@ -11,11 +11,11 @@ exports.seed = function (knex) {
         { id: 5, greeting: 'Salve', language: 'Italian', pronounciation: 'Sàl·ve' },
         { id: 6, greeting: 'Konnichiwa', language: 'Nihongo', pronounciation: 'Kon·nee·chee·wah' },
         { id: 7, greeting: 'Nǐ Hǎo', language: 'Mandarin', pronounciation: 'Nee·haow' },
-        { id: 8, greeting: 'Asalaam Alaikum', language: 'Arabic', pronounciation: 'as·sa·la·mu a·laj·kum' },
-        { id: 9, greeting: 'Guten Tag', language: 'Deutsch', pronounciation: 'gu·tn tak' },
-        { id: 10, greeting: 'добрий день', language: "Ukraïns'ka mova (Ukrainian)", pronounciation: 'dob·reh·dain' },
-        { id: 11, greeting: 'Namaste', language: 'Hindi', pronounciation: 'na·mas·tei' },
-        { id: 12, greeting: 'Annyeonghaseyo', language: 'Korean', pronounciation: 'an·yung·ha·say·o' }
+        { id: 8, greeting: 'Asalaam Alaikum', language: 'Arabic', pronounciation: 'As·sa·la·mu a·laj·kum' },
+        { id: 9, greeting: 'Guten Tag', language: 'Deutsch', pronounciation: 'Gu·tn tak' },
+        { id: 10, greeting: 'добрий день', language: "Ukraïns'ka mova (Ukrainian)", pronounciation: 'Dob·reh·dain' },
+        { id: 11, greeting: 'Namaste', language: 'Hindi', pronounciation: 'Na·mas·tei' },
+        { id: 12, greeting: 'Annyeonghaseyo', language: 'Korean', pronounciation: 'An·yung·ha·say·o' }
       ])
     })
 }
