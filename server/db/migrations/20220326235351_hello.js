ae7a090007a3785bf4667f90c37ee6a9771f2947
Geoffrey-Sciascia <gwsciascia@gmail.com>
2022-03-26 23:59:59 +1300

    fill hello migration

--- server/db/migrations/20220326235351_hello.js
+++ server/db/migrations/20220326235351_hello.js
@@ -1,8 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('hello', (table) => {
+    table.increments('id')
+    table.string('greeting')
+    table.string('language')
+  })
+}
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('hello')
+}
